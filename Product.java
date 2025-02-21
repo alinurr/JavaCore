cb6ea548800d8c0d17796ee911bb5a3326b115b8
Raiymbekova Alinur <alinurraiymbekova@gmail.com>
2025-02-21 10:20:54 +0500

    change search method

--- Product.java
+++ Product.java
@@ -4,7 +4,11 @@ public class Product {
     int quantity;
     String category;
 
-
+    public void search(String productName) {
+        if(productName.equals(name)){
+            System.out.println(name + " " + price + " " + quantity + " " + category);
+        }
+    }
 
     public static void main(String[] args){
         Product espresso = new Product();
@@ -14,28 +18,27 @@ public class Product {
         espresso.category = "coffee";
 
         Product cappuccino = new Product();
+        cappuccino.name = "Espresso";
         cappuccino.price = 6.5;
         cappuccino.quantity = 1;
         cappuccino.category = "coffee";
 
         Product croissant = new Product();
+        croissant.name = "croissant";
         croissant.price = 7.0;
         croissant.quantity = 3;
         croissant.category = "bakery";
 
         Product pie = new Product();
+        pie.name = "Cheesecake";
         pie.price = 4.0;
         pie.quantity = 2;
         pie.category = "bakery";
 
         Product[] products = {espresso, cappuccino, croissant, pie};
 
-        public static void search(String productName) {
-            for (Product p : products){
-                if (p.name.equals(productName)){
-                    System.out.println(p);
-                }
-            }
+        for (Product product : products){
+            product.search("Espresso");
         }
 
     }
